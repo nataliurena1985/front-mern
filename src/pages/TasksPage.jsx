bb89af2ecd52a27cc4a1fb183b34aabea74f27de
claudia <claudianataliurena@gmail.com>
2024-05-26 12:16:21 -0300

    map

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getTasksRequest } from "../api/tasks.api";
 export default function TasksPage() {
+  const [tasks, setTasks] = useState([]);
+
   useEffect(() => {
     async function loadTasks() {
       const response = await getTasksRequest();
-      console.log(response);
+      console.log(response.data);
+      setTasks(response.data);
     }
     loadTasks();
   }, []);
@@ -12,6 +15,17 @@ export default function TasksPage() {
   return (
     <div>
       <h1>Tasks</h1>
+
+      {    tasks.map((task) => {
+           return       <div key={task.id}>
+                    <h2>{task.title}</h2>
+                    <p>{task.description}</p>
+                    <span>{task.done == 1 ? "️✅️" : "❌"}</span>
+
+                    <span>{task.createAt}</span>
+                  </div>;
+      })}
+
     </div>
   );
 }
