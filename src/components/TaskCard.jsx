bac38b4ba2a3c66c2c97396f2d6dd02113a4dbac
claudia <claudianataliurena@gmail.com>
2024-05-27 18:22:46 -0300

    elimino una tarea

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,4 +1,19 @@
+import { deleteTaskRequest } from "../api/tasks.api";
+
 const TaskCard = ({ task }) => {
+  const handleDelete = async(id) => {
+
+    try {
+      const response = await deleteTaskRequest(id);
+      console.log(response);
+      
+    } catch (error) {
+      console.error(error);
+    }
+
+
+  };
+
   return (
     // <div key={task.id} >
     <div>
@@ -7,7 +22,14 @@ const TaskCard = ({ task }) => {
       <span>{task.done == 1 ? "️✅️" : "❌"}</span>
 
       <span>{task.createAt}</span>
-      <button>Delete</button>
+      <button
+        onClick={() => {
+          //console.log(task.id);
+          handleDelete(task.id);
+        }}
+      >
+        Delete
+      </button>
       <button>Edit</button>
     </div>
   );
